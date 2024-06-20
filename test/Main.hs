513ff5b919b25983df95772e33a2abf79f99f11e
Futar <Futarimiti@163.com>
2024-06-20 16:32:45 +0100

    add test example

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,17 @@
 module Main (main) where
 
+import qualified System.Exit as Exit
+import           Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    result <- runTestTT tests
+    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
+
+tests :: Test
+tests = TestList
+  [ TestLabel "example test" exampleTest
+  ]
+
+exampleTest :: Test
+exampleTest = TestCase (pure ())
