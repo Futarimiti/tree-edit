aca5138436795fa75eea6375e22d3bddaf38d101
Futar <Futarimiti@163.com>
2024-06-21 16:48:47 +0100

    fix deriving warning

--- lib/System/File/Tree/Exceptions.hs
+++ lib/System/File/Tree/Exceptions.hs
@@ -3,4 +3,6 @@ module System.File.Tree.Exceptions where
 import Control.Exception
 
 newtype IllegalArgument = IllegalArgument String
-  deriving (Show, Eq, Ord, Read, Exception)
+  deriving (Show, Eq, Ord, Read)
+
+instance Exception IllegalArgument
