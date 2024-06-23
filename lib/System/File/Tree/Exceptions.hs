07199d745b18dcd4c391ff50d5eccf194e45f9e2
Futar <Futarimiti@163.com>
2024-06-23 14:45:23 +0100

    add type mismatch exception

--- lib/System/File/Tree/Exceptions.hs
+++ lib/System/File/Tree/Exceptions.hs
@@ -6,3 +6,8 @@ newtype IllegalArgument = IllegalArgument String
   deriving (Show, Eq, Ord, Read)
 
 instance Exception IllegalArgument
+
+newtype TypeMismatch = TypeMismatch String
+  deriving (Show, Eq, Ord, Read)
+
+instance Exception TypeMismatch
